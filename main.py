49d7a06205d6a11e17efba1e3d72db6b1de0e35e
adhirat <admin@adhirat.com>
2024-05-08 20:55:57 +1000

    Updating API
    Updating API

--- main.py
+++ main.py
@@ -4,10 +4,19 @@ import gaurabda as gcal
 import json as json
 import os as os
 import requests
+import io as io
 
 app = Flask(__name__)
 
 gcal.SetFastingSchema(0)
+
+# Function to extract attributes from an object
+def extract_attributes(obj):
+    attributes = {}
+    for attr in dir(obj):
+        if not attr.startswith('__') and not callable(getattr(obj, attr)):
+            attributes[attr] = getattr(obj, attr)
+    return attributes
 
 
 # List Time Zones
@@ -54,17 +63,6 @@ def list_location():
 @app.route('/countries', methods=['GET'])
 def get_countries():
     return jsonify(gcal.GetCountries()), 200
-
-
-# Function to extract attributes from an object
-def extract_attributes(obj):
-    attributes = {}
-    for attr in dir(obj):
-        if not attr.startswith('__') and not callable(getattr(obj, attr)):
-            attributes[attr] = getattr(obj, attr)
-    return attributes
-
-
 
 
 
@@ -171,6 +169,116 @@ def get_calculate(location,format):
             
     return send_file(filename, mimetype=returntype, as_attachment=True)   , 200
     #return jsonify(repr(tc.writeTableHtml)), 200
+
+
+@app.route('/calendar', methods=['GET','POST'])
+def getCalendar():
+    loca = {}
+    date = {}
+    period = None
+    fmt = None
+    req_data = None
+    if request.method == 'GET':
+        req_data = request.args
+    elif request.method == 'POST':
+        req_data = request.json
+    else:
+        return Flask.make_response('Unknown method', 500)
+
+    loca['city'] = req_data.get('city')
+    loca['country'] = req_data.get('country')
+    loca['latitude'] = req_data.get('latitude')
+    loca['longitude'] = req_data.get('longitude')
+    loca['tzname'] = req_data.get('tzname')
+    date['year'] = req_data.get('year')
+    date['month'] = req_data.get('month')
+    date['day'] = req_data.get('day')
+    period = req_data.get('period')
+    fmt = req_data.get('format')
+    
+    if loca['latitude'] is None and loca['longitude'] is not None \
+       or loca['latitude'] is not None and loca['longitude'] is None:
+       return Flask.make_response('Either both LATITUDE,LONGITUDE are valid or none of them', 500)
+
+    if loca['city'] is None:
+        return Flask.make_response('city: - Name of location must be specified.', 500)
+
+    if loca['country'] is None:
+        return Flask.make_response('country: Name of country must be specified.', 500)
+
+    if loca['latitude'] is None:
+        sp = gcal.FindLocation(city=loca['city'], country=loca['country'])
+        if sp is None:
+            return Flask.make_response('Location with name \'{}\', country \'{}\' is not found in database.'.format(loca['name'], loca['country']), 500)
+        loca['latitude'] = sp.m_fLatitude
+        loca['longitude'] = sp.m_fLongitude
+        loca['tzname'] = sp.m_strTimeZone
+        loca['location'] = sp
+    else:
+        loca['latitude'] = float(loca['latitude'])
+        loca['longitude'] = float(loca['longitude'])
+
+    if loca['tzname'] is None:
+        return Flask.make_response('tz: Name of timezone must be specified.', 500)
+    
+    if date['year'] is None:
+        d = io.Today()
+        date['year'] = d.year
+    else:
+        date['year'] = int(date['year'])
+
+    if date['month'] is None:
+        date['month'] = 1
+    else:
+        date['month'] = int(date['month'])
+
+    if date['day'] is None:
+        date['day'] = 1
+    else:
+        date['day'] = int(date['day'])
+
+    if period is None:
+        return Flask.make_response('p: Time period must be specified.', 500)
+    try:
+        period = int(period)
+    except:
+        return Flask.make_response('p: Time period is number of days (integer number).', 500)
+    if period<1:
+        return Flask.make_response('p: Time period must be greater than 0 days.', 500)
+    if period>3653:
+        return Flask.make_response('p: Time period must be lower than 3654 days.', 500)
+
+    tc = gcal.TCalendar()
+    date2 = gcal.GCGregorianDate(year=date['year'], month=date['month'], day=date['day'])
+    location = loca.get('location')
+    if location is None:
+        location = gcal.GCLocation(data={
+            'city': loca['city'],
+            'country': loca['country'],
+            'latitude': loca['latitude'],
+            'longitude': loca['longitude'],
+            'tzname': loca['tzname']
+        })
+    tc.CalculateCalendar(location,date2,period)
+
+    wf = io.StringIO()
+
+    # save results in various formats
+    if fmt == 'txt' or fmt=='text' or fmt=='plain':
+        tc.write(wf, format='plain')
+        return Response(wf.getvalue(), mimetype='text/plain')
+    elif fmt=='html':
+        tc.write(wf)
+        return Response(wf.getvalue(), mimetype='text/html')
+    elif fmt=='html-table':
+        tc.write(wf, layout='table')
+        return Response(wf.getvalue(), mimetype='text/html')
+    elif fmt=='xml':
+        tc.write(wf, format='xml')
+        return Response(wf.getvalue(), mimetype='text/xml')
+    else:
+        return jsonify(tc.get_json_object())
+
 
 @app.route('/data/<tab>', methods=['GET'])
 def invoke_api(tab):
